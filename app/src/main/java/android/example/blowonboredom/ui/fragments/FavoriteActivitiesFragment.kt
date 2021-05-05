8231b596df67506b8e9b46dd640fa76f73e9c9f3
Roman Makukha <roman.makuha@gmail.com>
2021-05-05 14:14:57 +0300

    Remove more menu from favorites item

--- app/src/main/java/android/example/blowonboredom/ui/fragments/FavoriteActivitiesFragment.kt
+++ app/src/main/java/android/example/blowonboredom/ui/fragments/FavoriteActivitiesFragment.kt
@@ -17,13 +17,12 @@ import androidx.recyclerview.widget.RecyclerView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class FavoriteActivitiesFragment : Fragment(R.layout.fragment_favorite_activities),
-    FavoriteActivitiesAdapter.FavoriteActivitiesListener {
+class FavoriteActivitiesFragment : Fragment(R.layout.fragment_favorite_activities) {
 
     private lateinit var adapter: FavoriteActivitiesAdapter
     private lateinit var recyclerView: RecyclerView
-    private val favoriteActivitiesVM : FavoriteActivitiesVM by viewModels()
-    private lateinit var items : MutableList<RandomActivity>
+    private val favoriteActivitiesVM: FavoriteActivitiesVM by viewModels()
+    private lateinit var items: MutableList<RandomActivity>
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -36,16 +35,17 @@ class FavoriteActivitiesFragment : Fragment(R.layout.fragment_favorite_activitie
     }
 
     private fun initSwipeToDelete() {
-        val itemTouchHelperCallback = object  : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+        val itemTouchHelperCallback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
             override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder): Boolean {
-              return false
+                return false
             }
 
             override fun onSwiped(viewHolder: RecyclerView.ViewHolder, position: Int) {
                 adapter.removeItem(viewHolder.adapterPosition)
-                showSnackBar("Delete activity?", "Undo", {
-                    adapter.undoRemoveItem()
-                }, {
+                showSnackBar(
+                        "Delete activity?",
+                        "Undo",
+                        { adapter.undoRemoveItem() }, {
                     adapter.getRemovedItem()?.key?.let { favoriteActivitiesVM.deleteFromFavoriteActivities(it) }
                 })
             }
@@ -55,24 +55,23 @@ class FavoriteActivitiesFragment : Fragment(R.layout.fragment_favorite_activitie
         itemTouchHelper.attachToRecyclerView(recyclerView)
     }
 
-
     private fun initObservers() {
         favoriteActivitiesVM.favoriteActivitiesState.observe(viewLifecycleOwner, { state ->
-            when(state) {
+            when (state) {
                 is FavoriteActivitiesState.Success<*> -> {
-                    if (state.data is List<*> ) {
+                    if (state.data is List<*>) {
                         val list = state.data as ArrayList<RandomActivity>
                         adapter.setItems(list)
                         items = list
                     }
                 }
                 is FavoriteActivitiesState.Error<*> -> {
-                   if(state.message is String) {
-                       showToast(state.message)
-                   }
+                    if (state.message is String) {
+                        showToast(state.message)
+                    }
                 }
                 is FavoriteActivitiesState.Complete<*> -> {
-                    if(state.message is String) {
+                    if (state.message is String) {
                         showSnackBar(state.message)
                     }
                 }
@@ -83,16 +82,10 @@ class FavoriteActivitiesFragment : Fragment(R.layout.fragment_favorite_activitie
 
     private fun initRecyclerView(view: View) {
         recyclerView = view.findViewById(R.id.fragment_favorite_activities_RV)
-        adapter = FavoriteActivitiesAdapter(this)
+        adapter = FavoriteActivitiesAdapter()
         val layoutManager = LinearLayoutManager(requireContext())
         recyclerView.adapter = adapter
         recyclerView.layoutManager = layoutManager
-
-    }
-
-    override fun onActivityClick(position: Int) {
-        adapter.removeItem( position)
-        //favoriteActivitiesVM.deleteFromFavoriteActivities(item.key)
 
     }
 
